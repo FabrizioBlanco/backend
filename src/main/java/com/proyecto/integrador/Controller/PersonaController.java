e4c603026719cca48a9d109ee370dbad90f2fcd7
FabrizioBlanco <Fabrizio.blanco2021@gmail.com>
2022-10-27 19:31:04 -0300

    Update PersonaController.java

--- src/main/java/com/proyecto/integrador/Controller/PersonaController.java
+++ src/main/java/com/proyecto/integrador/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
-@RequestMapping("/persona")
+@RequestMapping("/personas")
 //@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://porfoliofrontend.web.app")//sirve para que acepte las peticiones desde esa página
 public class PersonaController {
